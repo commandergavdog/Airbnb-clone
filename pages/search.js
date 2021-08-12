078785219a7d23b133d957e527b8344d7d385728
fdominguez2 <fdominguez@naic.org>
2021-08-11 22:02:29 -0500

    Search props
    pass search page # of guests, location, dates & include in search bar.

--- pages/search.js
+++ pages/search.js
@@ -1,17 +1,29 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useRouter } from "next/dist/client/router";
+import {format} from "date-fns";
 
 function Search() {
+    const router = useRouter();
+    // console.log(router.query);
+
+    //ES6 Destructuring
+    const {location, startDate, endDate, noOfGuests} = router.query;
+
+    const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+    const formattedendDate = format(new Date(endDate), "dd MMMM yy");
+    const range = `${formattedStartDate} - ${formattedendDate}`
+
     return (
         <div className="">
-            <Header />
+            <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`}/>
 
 
             <main className="flex">
-                <section>
-                    <p className="text-xs">300+ Stays - 5 guests</p>
+                <section className="flex-grow pt-14 px-6">
+                    <p className="text-xs">300+ Stays - {range} - for {noOfGuests} guests</p>
 
-                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
 
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button">Cancellation Flexibility</p>
