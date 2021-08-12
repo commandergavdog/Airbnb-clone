22b952cddb4e8c05968111f3c72d8a9c5893ded5
fdominguez2 <fdominguez@naic.org>
2021-08-11 21:25:02 -0500

    Search page button styles
    Added search page and filter button styles

---
+++ pages/search.js
@@ -0,0 +1,31 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Search() {
+    return (
+        <div className="">
+            <Header />
+
+
+            <main className="flex">
+                <section>
+                    <p className="text-xs">300+ Stays - 5 guests</p>
+
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+
+                    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                        <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms and Beds</p>
+                        <p className="button">More filters</p>                                            
+                    </div>
+                </section>
+
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Search
